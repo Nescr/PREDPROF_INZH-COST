74a32c4763b7990a55de895d83918d9e04329bf8
Nescr <73983630+Nescr@users.noreply.github.com>
2023-03-28 20:12:19 +0300

    Update narabotki_conv.ino

--- конвейер - ПО/narabotki_conv.ino
+++ конвейер - ПО/narabotki_conv.ino
@@ -37,6 +37,9 @@ Adafruit_NeoPixel strip(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
 // пин концевого датчика с противоположной стороны от мотора сортирующей системы
 #define SECOND_BUTTON D1
 
+//
+#define COLORS_NUM_FOR_START 2
+
 // переменные для хранения данных о выбранном цвете
 int red   = 0;
 int green = 0;
@@ -54,14 +57,72 @@ EspMQTTClient client(
   "23124"
 );
 
+//
+int check_colors_col()
+{
+  int sum = red + green + blue + white;
+  if (sum == COLORS_NUM_FOR_START){
+    Serial.println("Успешно выбраны " + String(COLORS_NUM_FOR_START) + " цвета! Устройство готово к запускау!");
+    client.publish("user/NESCR/message", "Успешно выбраны 2 цвета! Устройство готово к запускау!");
+    return 0;
+  }
+  else if (sum > COLORS_NUM_FOR_START){
+    Serial.println("Выбрано слишком много цветов. Для запуска нужно выбрать строго 2 цвета!!");
+    client.publish("user/NESCR/message", "Успешно выбраны 2 цвета! Устройство готово к запускау!");
+    return 1;
+  }
+  return 0;
+}
+
+void check_colors_start()
+{
+  int sum = red + green + blue + white;
+  if (sum == COLORS_NUM_FOR_START){
+    Serial.println("Успешно выбраны " + String(COLORS_NUM_FOR_START) + " цвета! Запуск системы!");
+    client.publish("user/NESCR/message", "Успешно выбраны " + String(COLORS_NUM_FOR_START) + " цвета! Запуск системы!");
+  }
+  else if (sum > COLORS_NUM_FOR_START){
+    Serial.println("Выбрано слишком много цветов. Для запуска нужно выбрать строго 2 цвета!!");
+    client.publish("user/NESCR/message", "Успешно выбраны " + String(COLORS_NUM_FOR_START) + " цвета! Устройство готово к запускау!");
+  }
+  ////////
+}
+
 // подписка на топики
 void onConnectionEstablished() 
 { 
-  client.subscribe("/user/NESCR/red",   [] (const String &payload) { red   = payload.toInt(); }); 
-  client.subscribe("/user/NESCR/green", [] (const String &payload) { green = payload.toInt(); }); 
-  client.subscribe("/user/NESCR/blue",  [] (const String &payload) { blue  = payload.toInt(); }); 
-  client.subscribe("/user/NESCR/white", [] (const String &payload) { white = payload.toInt(); }); 
-  client.subscribe("/user/NESCR/start", [] (const String &payload) { start = payload.toInt(); }); 
+  client.subscribe("/user/NESCR/red",   [] (const String &payload) {
+    red   = payload.toInt();
+    if (check_colors_col() == 1)
+    {
+      client.publish("/user/NESCR/red", "0"); 
+    }
+  }); 
+  client.subscribe("/user/NESCR/green", [] (const String &payload) {
+    green = payload.toInt();
+    if (check_colors_col() == 1)
+    {
+      client.publish("/user/NESCR/green", "0"); 
+    }
+  }); 
+  client.subscribe("/user/NESCR/blue",  [] (const String &payload) {
+    blue  = payload.toInt();
+    if (check_colors_col() == 1)
+    {
+      client.publish("/user/NESCR/blue", "0"); 
+    }
+  });
+  client.subscribe("/user/NESCR/white", [] (const String &payload) {
+    white = payload.toInt();
+    if (check_colors_col() == 1)
+    {
+      client.publish("/user/NESCR/white", "0"); 
+    }
+  });
+  client.subscribe("/user/NESCR/start", [] (const String &payload) {
+    start = payload.toInt();
+    check_colors_start();
+  }); 
 } 
 
 // первоначальная настройка светодиодной ленты
