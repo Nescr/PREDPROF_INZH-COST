b35c2d4f07e417a2f79ebe359de2604cb9ed03fe
Nescr <73983630+Nescr@users.noreply.github.com>
2023-04-04 20:51:10 +0300

    Update proverka_motor_conv.ino.ino

--- конвейер - ПО/proverka_motor_conv.ino/proverka_motor_conv.ino.ino
+++ конвейер - ПО/proverka_motor_conv.ino/proverka_motor_conv.ino.ino
@@ -1,45 +1,37 @@
-#define STEP_CONV     7
-#define DIR_CONV      6
-#define EN_PIN 8
+#define STEP_CONV     13
+#define DIR_CONV      15
 
-void conv_forward()
-{
-  digitalWrite(DIR_CONV , HIGH);
-  digitalWrite(STEP_CONV, HIGH); 
-  delayMicroseconds(200);
-  
-  digitalWrite(STEP_CONV, LOW); 
-  delayMicroseconds(200);
-}
 
 void conv_backward()
 {
+  digitalWrite(DIR_CONV , HIGH);
+  digitalWrite(STEP_CONV, HIGH); 
+  delayMicroseconds(500);
+  
+  digitalWrite(STEP_CONV, LOW); 
+  delayMicroseconds(500);
+}
+
+void conv_forward()
+{
 digitalWrite(DIR_CONV , LOW);
   digitalWrite(STEP_CONV, HIGH); 
-  delayMicroseconds(200);
+  delayMicroseconds(500);
 
   digitalWrite(STEP_CONV, LOW); 
-  delayMicroseconds(200);
+  delayMicroseconds(500);
 }
 
 void setup()
 {
   pinMode(DIR_CONV, OUTPUT);
   pinMode(STEP_CONV, OUTPUT);
-  pinMode(EN_PIN, OUTPUT);
-  digitalWrite(EN_PIN, LOW);
 }
 
 void loop()
 {
-  
   for (int i=0; i < 1000; i++)
   {
     conv_forward();
-  }
-
-  for (int i=0; i < 1000; i++)
-  {
-    conv_backward();
   }
 }
